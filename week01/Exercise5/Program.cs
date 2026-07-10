8a7a7b72fe2d389884e5bbe2a7ed706ff18ddc20
ezeprosperchibueze <ezeprosperchibueze@gmail.com>
2026-07-10 23:48:40 +0100

    commit changes

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,50 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        // Call each function in order
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int favoriteNumber = PromptUserNumber();
+
+        int squaredNumber = SquareNumber(favoriteNumber);
+
+        DisplayResult(userName, squaredNumber);
+    }
+
+    // Function to display welcome message
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    // Function to prompt for user's name
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    // Function to prompt for user's favorite number
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    // Function to square a number
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    // Function to display the result
+    static void DisplayResult(string name, int squaredNumber)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
     }
 }
