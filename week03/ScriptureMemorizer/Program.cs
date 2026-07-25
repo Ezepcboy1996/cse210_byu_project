b1455baf1a014b5e742483f414a085f77cb2de35
ezeprosperchibueze <ezeprosperchibueze@gmail.com>
2026-07-25 14:21:36 +0100

    add changes

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,9 +1,27 @@
 using System;
 
-class Program
+namespace Week03Project
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        static void Main(string[] args)
+        {
+            // Test all constructors
+            Fraction f1 = new Fraction();          // 1/1
+            Fraction f2 = new Fraction(5);         // 5/1
+            Fraction f3 = new Fraction(3, 4);      // 3/4
+            Fraction f4 = new Fraction(1, 3);      // 1/3
+
+            // Display results
+            Console.WriteLine("Fraction f1: " + f1.GetFractionString() + " = " + f1.GetDecimalValue());
+            Console.WriteLine("Fraction f2: " + f2.GetFractionString() + " = " + f2.GetDecimalValue());
+            Console.WriteLine("Fraction f3: " + f3.GetFractionString() + " = " + f3.GetDecimalValue());
+            Console.WriteLine("Fraction f4: " + f4.GetFractionString() + " = " + f4.GetDecimalValue());
+
+            // Demonstrate setters
+            f4.SetTop(2);
+            f4.SetBottom(5);
+            Console.WriteLine("Updated f4: " + f4.GetFractionString() + " = " + f4.GetDecimalValue());
+        }
     }
 }
